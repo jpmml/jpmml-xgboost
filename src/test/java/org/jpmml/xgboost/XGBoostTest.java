b52eb0e819876cfd2df8ccfb7d5f7023130268a7
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-02-26 15:59:32 +0200

    Ensured the validity of PMML class model objects

--- src/test/java/org/jpmml/xgboost/XGBoostTest.java
+++ src/test/java/org/jpmml/xgboost/XGBoostTest.java
@@ -1,13 +1,18 @@
 package org.jpmml.xgboost;
 
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import org.dmg.pmml.FieldName;
 import org.dmg.pmml.PMML;
+import org.dmg.pmml.Visitor;
 import org.jpmml.evaluator.ArchiveBatch;
 import org.jpmml.evaluator.Batch;
 import org.jpmml.evaluator.IntegrationTest;
+import org.jpmml.evaluator.visitors.InvalidFeatureInspector;
+import org.jpmml.evaluator.visitors.UnsupportedFeatureInspector;
 
 public class XGBoostTest extends IntegrationTest {
 
@@ -38,6 +43,8 @@ public class XGBoostTest extends IntegrationTest {
 
 				PMML pmml = learner.encodePMML(featureMap);
 
+				ensureValidity(pmml);
+
 				return pmml;
 			}
 		};
@@ -48,5 +55,17 @@ public class XGBoostTest extends IntegrationTest {
 	@Override
 	public void evaluate(Batch batch, Set<FieldName> ignoredFields) throws Exception {
 		evaluate(batch, ignoredFields, 1e-6, 1e-6);
+	}
+
+	static
+	private void ensureValidity(PMML pmml){
+		List<Visitor> visitors = Arrays.<Visitor>asList(
+			new UnsupportedFeatureInspector(),
+			new InvalidFeatureInspector()
+		);
+
+		for(Visitor visitor : visitors){
+			visitor.applyTo(pmml);
+		}
 	}
 }
